ec6a5e62edf63eed6809899a6d7141da0fc26363
Marie Rivier <marie.rivier23@gmail.com>
2025-06-19 16:40:09 +0200

    formulaire read & create & success

--- src/Controller/AddGlaceController.php
+++ src/Controller/AddGlaceController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Glaces;
+use App\Form\GlacesTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class AddGlaceController extends AbstractController
 {
     #[Route('/add/glace', name: 'add_glace')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $glace = new Glaces();
+        $form = $this->createForm(GlacesTypeForm::class, $glace);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($glace);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Glace ajoutée avec succès !');
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('add_glace/addGlace.html.twig', [
-            'controller_name' => 'AddGlaceController',
+            'glaceForm' => $form->createView(),
         ]);
     }
 }
--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GlacesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ListeController extends AbstractController
 {
     #[Route('/liste', name: 'liste')]
-    public function index(): Response
+    public function index(GlacesRepository $repository): Response
     {
+        $glaces = $repository->findAll();
+
         return $this->render('liste/liste.html.twig', [
-            'controller_name' => 'ListeController',
+            'glaces' => $glaces,
         ]);
     }
 }
